ea701ff53263877d17afb9c83561f49acf9763f1
Aris <frestaris@gmail.com>
2025-01-04 18:16:32 +1000

    removed / from cors

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "https://mern-car-rent.netlify.app/",
+    origin: "https://mern-car-rent.netlify.app",
     credentials: true,
   })
 );
