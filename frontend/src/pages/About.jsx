4fea05b391c9eb3fcd50a4aa22119296c0428939
Aris <frestaris@gmail.com>
2025-01-04 11:44:33 +1000

    added contact and about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,5 +1,139 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaChevronDown } from "react-icons/fa";
+
+const faqData = [
+  {
+    question: "What is this rental service?",
+    answer:
+      "Our rental service provides a wide range of vehicles for rent, with flexible pickup and drop-off options.",
+  },
+  {
+    question: "How can I make a booking?",
+    answer:
+      "You can make a booking by selecting your desired car, entering the dates, and completing the payment process.",
+  },
+  {
+    question: "Are there any additional fees?",
+    answer:
+      "We charge a small processing fee for each rental, but no hidden charges are applied.",
+  },
+  {
+    question: "What is the cancellation policy?",
+    answer:
+      "You can cancel your booking up to 24 hours before the pickup time to receive a full refund.",
+  },
+  {
+    question: "What types of vehicles are available for rent?",
+    answer:
+      "We offer a wide selection of vehicles, including sedans, SUVs, trucks, and luxury cars. You can choose based on your needs and preferences.",
+  },
+  {
+    question: "Is insurance included in the rental?",
+    answer:
+      "Yes, basic insurance is included in the rental. However, additional coverage options are available at an extra cost.",
+  },
+  {
+    question: "Can I extend my rental period?",
+    answer:
+      "Yes, you can extend your rental period. Please contact us at least 24 hours before your return time to adjust your rental period.",
+  },
+  {
+    question: "Do I need to pay a deposit?",
+    answer:
+      "Yes, a refundable deposit is required at the time of booking. The deposit will be refunded once the vehicle is returned in good condition.",
+  },
+];
+
 const About = () => {
-  return <div>About</div>;
+  const [open, setOpen] = useState(null);
+
+  const toggleAccordion = (index) => {
+    setOpen(open === index ? null : index);
+  };
+
+  return (
+    <div className="bg-gray-100">
+      {/* Hero Section */}
+      <section className=" py-8">
+        <div className="max-w-7xl mx-auto flex flex-col-reverse lg:flex-row items-center justify-between px-6">
+          {/* Image Section */}
+          <div className="w-full lg:w-1/2">
+            <img
+              src="https://images.pexels.com/photos/15160474/pexels-photo-15160474/free-photo-of-aerial-view-of-car-driving-through-road-in-snowy-valley.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="Hero"
+              className="w-full h-auto rounded-lg shadow-lg"
+            />
+          </div>
+
+          {/* Text Section */}
+          <div className="w-full lg:w-1/2 pl-8">
+            <h1 className="text-4xl font-semibold text-gray-800 mb-4">
+              Rent Your Dream Car Today
+            </h1>
+            <p className="text-lg text-gray-600 mb-6">
+              Choose from a wide range of vehicles to make your next adventure
+              unforgettable. Fast, reliable, and affordable.
+            </p>
+            <Link
+              to="/vehicles"
+              className="inline-block bg-amber-600 text-white px-6 py-3 rounded-lg shadow-lg hover:bg-amber-700 transition"
+            >
+              Explore Our Fleet
+            </Link>
+            <p className="my-6 text-md text-gray-600">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam
+              rerum voluptas culpa incidunt facilis minima sed, veritatis,
+              commodi pariatur consequatur quibusdam! Perferendis totam quos
+              voluptatibus facere sint. Debitis, quis accusamus. Itaque at
+              voluptatibus deleniti adipisci illum iusto porro quos qui. Magnam
+              ex itaque tempore maxime cupiditate in, recusandae, exercitationem
+              harum molestias libero magni, quos assumenda aliquam culpa illum
+              mollitia reiciendis? Exercitationem sint accusamus ullam maiores?
+              Mollitia sequi, esse id animi vitae minus veniam ratione quibusdam
+              expedita illum delectus itaque quos, sed officia. Ducimus,
+              doloribus excepturi ex eaque similique labore voluptates. Aut
+            </p>
+          </div>
+        </div>
+      </section>
+
+      {/* FAQ Section */}
+      <section className="max-w-3xl mx-auto p-6">
+        <h2 className="text-2xl font-semibold mb-6 text-center">
+          Frequently Asked Questions
+        </h2>
+        <div>
+          {faqData.map((item, index) => (
+            <div key={index} className="border border-gray-200">
+              <div
+                className={`p-4 cursor-pointer flex justify-between items-center ${
+                  open === index ? "bg-gray-200" : "bg-white"
+                }`}
+                onClick={() => toggleAccordion(index)}
+              >
+                <h3 className="text-lg font-medium text-gray-800">
+                  {item.question}
+                </h3>
+                <div
+                  className={`transform transition-transform duration-500 ease-in-out ${
+                    open === index ? "rotate-180" : "rotate-0"
+                  }`}
+                >
+                  <FaChevronDown className="text-gray-600" />
+                </div>
+              </div>
+              {open === index && (
+                <div className="p-4 bg-white">
+                  <p className="text-gray-700">{item.answer}</p>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default About;
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,5 +1,139 @@
+import { useState } from "react";
+import Swal from "sweetalert2";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+    inquiryType: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (formData.name && formData.email && formData.message) {
+      Swal.fire({
+        title: "Thank you!",
+        text: "Your message has been sent successfully!",
+        icon: "success",
+        confirmButtonText: "Close",
+      });
+
+      setFormData({
+        name: "",
+        email: "",
+        message: "",
+        inquiryType: "",
+      });
+    } else {
+      Swal.fire({
+        title: "Error!",
+        text: "Please fill out all the fields.",
+        icon: "error",
+        confirmButtonText: "Try Again",
+      });
+    }
+  };
+
+  return (
+    <section className="max-w-4xl mx-auto p-6">
+      <h2 className="text-2xl font-semibold mb-6 text-center">Contact Us</h2>
+      <p className="text-center text-lg text-gray-600 mb-8">
+        Need help or have any questions? Reach out to our team directly via the
+        contact form below, or find more details on how to reach us.
+      </p>
+
+      {/* Call Us Section */}
+      <div className="mb-8">
+        <h3 className="text-xl font-semibold text-gray-800 mb-4">Call Us</h3>
+        <p className="text-gray-600 mb-4">
+          For Domestic Reservations or Local Reservations, use the following
+          number:
+        </p>
+        <p className="text-gray-700 font-semibold">Phone Number: 12 34 56</p>
+        <p className="text-gray-600 mb-4">
+          Hours of Operation: Mon-Sun: 08:00-20:00
+        </p>
+        <p className="text-gray-600 mb-4">Additional Service Numbers:</p>
+        <ul className="list-disc list-inside text-gray-700">
+          <li>Gold Plus Rewards: 1234 567 890</li>
+          <li>Wheelchair Accessible Vehicles: 1234 567 890</li>
+          <li>
+            Claims Management (Email for Damage claims only): whatever@email.com
+          </li>
+          <li>Customer Relations (For Past Rentals Only): 1234 567 890</li>
+        </ul>
+      </div>
+      <div className="mb-8 text-center text-lg font-semibold text-gray-700">
+        <p>For other inquiries, submit this form:</p>
+      </div>
+      {/* Form Section */}
+      <form onSubmit={handleSubmit} className="space-y-6">
+        <div className="flex flex-col">
+          <label htmlFor="name" className="text-lg font-medium text-gray-700">
+            Your Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+            required
+            className="mt-2 p-3 border border-gray-300 rounded-lg focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-amber-600"
+          />
+        </div>
+
+        <div className="flex flex-col">
+          <label htmlFor="email" className="text-lg font-medium text-gray-700">
+            Your Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            required
+            className="mt-2 p-3 border border-gray-300 rounded-lg focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-amber-600"
+          />
+        </div>
+
+        <div className="flex flex-col">
+          <label
+            htmlFor="message"
+            className="text-lg font-medium text-gray-700"
+          >
+            Your Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleInputChange}
+            required
+            rows="6"
+            className="mt-2 p-3 border border-gray-300 rounded-lg focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-amber-600"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="w-full bg-amber-600 text-white px-6 py-3 rounded-lg shadow-lg hover:bg-amber-700 transition"
+        >
+          Submit
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default Contact;
