8d070d7ff6b13e3171efa5592bf1be7f0448eb03
Aris <frestaris@gmail.com>
2025-01-05 06:21:37 +1000

    vercel frontend installation

--- frontend/src/components/PaymentSuccess.jsx
+++ frontend/src/components/PaymentSuccess.jsx
@@ -23,7 +23,7 @@ const PaymentSuccess = () => {
         setLoading(true);
         hasCreatedBooking.current = true;
         try {
-          const response = await axios.get(
+          const response = await axios.post(
             `${baseURL}/api/booking/payment-success?session_id=${sessionId}`,
             config
           );
